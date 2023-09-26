dc55823fb33368c0406809af7ebd53fb56ebd5c8
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2023-09-26 12:28:43 +0700

    Add "Change Email" feature request models

---
+++ Api/Models/Account/ChangeEmailRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models.Account
+{
+    public class ChangeEmailRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string? NewEmail { get; set; }
+    }
+}
