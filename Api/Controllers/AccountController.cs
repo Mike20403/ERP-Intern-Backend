9730272ef88c71db57b5292808cf15dde3c3d177
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2023-09-26 12:26:53 +0700

    Add "Change Email" feature controller routes

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -2,6 +2,9 @@
 using Api.Dtos;
 using Api.Models.Account;
 using AutoMapper;
+using DotNetStarter.Commands.Account.ActivateEmail;
+using DotNetStarter.Commands.Account.ConfirmChangeEmail;
+using DotNetStarter.Commands.Account.ChangeEmailRequires;
 using DotNetStarter.Commands.Users.Update;
 using DotNetStarter.Extensions;
 using DotNetStarter.Queries.Users.Get;
@@ -43,6 +46,32 @@ namespace Api.Controllers
             var result = await _mediator.Send(new UpdateUser(user.Id, request.Firstname!, request.Lastname!, request.PhoneNumber!, user.Status!.Value));
 
             return Ok(_mapper.Map<AccountDto>(result));
+        }
+
+        [HttpPost("request-change-email")]
+        public async Task<ActionResult> RequestChangeEmail(ChangeEmailRequest request) // Request to change email then recieve active code via current email
+        {
+            var user = await _mediator.Send(new GetUser(HttpContext.GetCurrentUserId()!.Value));
+
+            await _mediator.Send(new RequestChangeEmail(user.Username!, request.NewEmail!));
+
+            return Ok();
+        }
+
+        [HttpPost("change-email")]
+        public async Task<ActionResult> ConfirmChangeEmail(ConfirmChangeEmailRequest request) // Change Email via active code then set user to inactice
+        {
+            await _mediator.Send(new ConfirmChangeEmail(request.CurrentEmail!, request.NewEmail!, request.ActiveCode!));
+
+            return Ok($"Email is changed into {request.NewEmail}, please check the inbox and activate your account soon");
+        }
+
+        [HttpPost("activate-email")]
+        public async Task<ActionResult> ActivateNewEmail(ActivateEmailRequest request) // Activate new Email after change email by any one has authorized
+        {
+            await _mediator.Send(new ActivateEmail(request.Email!, request.ActiveCode!));
+
+            return Ok("Account Activated!");
         }
     }
 }
